7687fe7809ce78ffe6f8d059d64b90f6d2d74a3d
Uk_ash <ukasek1986@googlemail.com>
2023-02-08 19:14:12 +0000

    pd d1

--- NewChallengeApp/Program.cs
+++ NewChallengeApp/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string name = "Lukasz";
+char sex = 'M';
+int age = 33;
+
+if (sex != 'M')
+{
+    Console.WriteLine("to jest kobieta");
+}
+else if (sex == 'M')
+{
+    Console.WriteLine("to jest mężczyzna");
+    if (name == "Lukasz")
+    {
+        Console.WriteLine("to jest mężczyzna o imieniu " + name);
+        if (age == 33)
+        {
+            Console.WriteLine("to jest mężczyzna o imieniu " + name + " który ma " + age + " lat.");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("nie wiadomo kto to");
+}
