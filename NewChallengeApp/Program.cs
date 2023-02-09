e640b6b3ddd9f237d98dfce121ef6d364491c0e2
Uk_ash <ukasek1986@googlemail.com>
2023-02-09 20:39:33 +0000

    pd d5

--- NewChallengeApp/Program.cs
+++ NewChallengeApp/Program.cs
@@ -1,14 +1,32 @@
-﻿int number = 993692331;
-char[] numbers = number.ToString().ToArray();
+﻿long numbers = 7666689993322173223;
+char[] chars = numbers.ToString().ToCharArray();
 string digits = "0123456789";
 
-foreach (var digit in digits)
+Console.WriteLine("Wynik dla liczby 7666689993322173223");
+Console.WriteLine();
+
+foreach (char digit in digits)
 {
     var times = 0;
-    foreach (var numberInNumbers in numbers)
+        foreach (var number in chars)
     {
-        if (numberInNumbers == digit)
+        if (number == digit)
             times++;
     }
     Console.WriteLine(digit + " => " + times);
 }
+
+//int number = 993692331;
+//char[] numbers = number.ToString().ToArray();
+//string digits = "0123456789";
+
+//foreach (var digit in digits)
+//{
+//    var times = 0;
+//    foreach (var numberinnumbers in numbers)
+//    {
+//        if (numberinnumbers == digit)
+//            times++;
+//    }
+//    Console.WriteLine(digit + " => " + times);
+//}
